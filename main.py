2306108aee9e79059ede6aee937a65f247689691
SandroSH <sandro.shav@gmail.com>
2024-12-17 19:41:09 +0400

    Upload project

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-print('Git example')
-
-# git init
-# git remote add origin https://github.com/ShavdatuashviliSandro/sandros-assignments.git
-# git add .
-
-
-####
+for i in range(1,300):
+    print(i)
